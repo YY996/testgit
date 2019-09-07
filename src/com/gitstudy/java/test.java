88ce94a00010e4aebb60e0630265b8759dfd4591
YY99602 <15890542076@myoas.com>
2019-09-07 14:37:18 +0800

    提交222222

--- src/com/gitstudy/java/test.java
+++ src/com/gitstudy/java/test.java
@@ -3,5 +3,6 @@ package com.gitstudy.java;
 public class test {
     public void main (String[] args) {
         System.out.println("1111111111111111");
+        System.out.println("2222222222222222");
     }
 }
